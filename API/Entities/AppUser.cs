2b83fb5b27ccc0053359c4f6bcf63a712864cc0f
Clodagh O'Neill <coneill165@qub.ac.uk>
2021-03-29 19:15:44 +0100

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,22 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        // EF Features
+        // Object Relational Mapper (ORM)
+        // Translates code into SQL
+        // Allows wquerying using LINQ
+        // Change Tracking
+        // Saving
+        // Concurrency
+        // Transations
+        // Caching
+        // Built in conventions
+        // Configurations
+        // Migrations
+    }
+}
